492ca9c6d20f53d6f89e2b189feb0b005dfae116
HIRAYAMA\reona <belinklab@gmail.com>
2025-08-06 14:33:00 +0900

    キャリアページ作成

---
+++ page-career.php
@@ -0,0 +1,297 @@
+<?php
+if (! defined('ABSPATH')) exit;
+/* 
+Template Name: キャリア採用ページ
+*/
+?>
+
+<?php get_template_part('template-parts/header'); ?>
+<?php get_template_part('template-parts/components/page-head', null, [
+    'title_en' => 'CAREER',
+    'title_jp' => 'キャリア採用',
+]); ?>
+<div class="section mt160--sp96">
+    <div class="txt">
+        <p>
+            近江印刷株式会社では、キャリア採用を行っております。<br>
+            ご興味のある方は、応募要項をご確認のうえ、<a href="" class="link-underline fw400" target="_blank">下記エントリーフォーム</a>からご応募ください。
+        </p>
+    </div>
+    <div class="accordion-list mt128--sp72">
+        <div class="faq-item m0">
+            <p class="faq-title faq-title--no-border-radius faq-title--btn jcc">共通要項</p>
+            <dl class="faq-content mt64--sp40">
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        雇用形態
+                    </dt>
+                    <dd>
+                        正社員
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        勤務時間
+                    </dt>
+                    <dd>
+                        8：35～17：35（休憩１時間）
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        勤務地
+                    </dt>
+                    <dd>
+                        本社<br>
+                        〒529-1302 滋賀県愛知郡愛荘町川原771-1
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        休日・休暇
+                    </dt>
+                    <dd>
+                        基本 土日祝休み（会社カレンダー通り）<br>
+                        年間休日120日（2026年度）<br>
+                        年末年始休暇、GW休暇、夏季休暇、介護休暇制度・出産育児休暇制度有
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        賞与・昇給
+                    </dt>
+                    <dd>
+                        賞与：年２回（夏・冬）<br>
+                        昇給：年１回
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        福利厚生
+                    </dt>
+                    <dd>
+                        <p><span>・&nbsp;</span>健康保険、厚生年金保険、雇用保険、労災保険</p>
+                        <p><span>・&nbsp;</span>定期健康診断</p>
+                        <p><span>・&nbsp;</span>退職金制度</p>
+                        <p><span>・&nbsp;</span>財形貯蓄</p>
+                        <p><span>・&nbsp;</span>慶弔見舞金制度</p>
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        採用プロセス
+                    </dt>
+                    <dd>
+                        ①エントリー受付<br>
+                        ②履歴書・ポートフォリオ提出<br>
+                        ③書類選考<br>
+                        ④一次面接<br>
+                        ⑤最終面接
+                    </dd>
+                </div>
+
+            </dl>
+        </div><!-- /.faq-item -->
+        <details class="faq-item">
+            <summary class="faq-title faq-title--btn jcc">
+                グラフィックデザイナー
+                <span class="faq-title__icon"></span>
+            </summary>
+            <dl class="faq-content mt64--sp40">
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        給与
+                    </dt>
+                    <dd>
+                        230,000円〜270,000円
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        仕事内容
+                    </dt>
+                    <dd>
+                        ・&nbsp;印刷物のデザイン制作
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        必須スキル
+                    </dt>
+                    <dd>
+                        ・&nbsp;Illustrator、Photoshopが扱える方
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        歓迎スキル
+                    </dt>
+                    <dd>
+                        <p><span>・&nbsp;</span>デザイン制作の実務経験1年以上、またはそれに準ずる経験</p>
+                        <p><span>・&nbsp;</span>DTP経験をお持ちの方</p>
+                        <p><span>・&nbsp;</span>inDesignが使用できる方</p>
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        主な製制作物
+                    </dt>
+                    <dd>
+                        チラシ、パンフレット、冊子類、広報誌など
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        使用ソフト
+                    </dt>
+                    <dd>
+                        <p><span>・&nbsp;</span>Adobe Illustrator（CC2020～2025）</p>
+                        <p><span>・&nbsp;</span>Adobe Photoshop（CC2020～2025）</p>
+                        <p><span>・&nbsp;</span>Adobe inDesign（CC2020～2025）</p>
+                        <p><span>・&nbsp;</span>Adobe Acrobat pro</p>
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        使用フォント
+                    </dt>
+                    <dd>
+                        モリサワパスポート
+                    </dd>
+                </div>
+            </dl>
+        </details><!-- /.faq-item -->
+
+        <details class="faq-item">
+            <summary class="faq-title faq-title--btn jcc">
+                Webデザイナー / Webコーダー
+                <span class="faq-title__icon"></span>
+            </summary>
+            <dl class="faq-content mt64--sp40">
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        給与
+                    </dt>
+                    <dd>
+                        230,000円〜270,000円
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        職種
+                    </dt>
+                    <dd>
+                        ①Webデザイナー<br>
+                        ②Webコーダー
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        仕事内容
+                    </dt>
+                    <dd>
+                        ①Webサイトのデザイン<br>
+                        ②Webサイトのコーディング・保守
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        必須スキル
+                    </dt>
+                    <dd>
+                        Web制作業務の実務経験1年以上<br>
+                        ①llustrator、Photoshopを扱える方<br>
+                        ②HTML5・CSS3、javascript、PHP（WordPress）
+                    </dd>
+                </div>
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        歓迎スキル
+                    </dt>
+                    <dd>
+                        ①AdobeXD、Figma、After Effects、Premiere Proを使える方<br>
+                        ②Webサイトの改善、SEO対策、システム管理の経験
+                    </dd>
+                </div>
+
+                <div class="faq-content__item-wrapper">
+                    <dt>
+                        主な制作物
+                    </dt>
+                    <dd>
+                        Webサイト制作（コーポレートサイト・ECサイト・LPページなど）、リニューアル、SNS集客、写真・動画制作
+                    </dd>
+                </div>
+            </dl>
+        </details><!-- /.faq-item -->
+        <!-- 取り扱い -->
+        <div class="active-policy">
+            <div class="active-policy-inner bgLightBlue">
+                <h3 class="active-policy-title">外部スタッフエントリーに<br class="mobile">関する個人情報の取り扱い</h3>
+                <p class="active-policy-text mt16">下記事項をご確認のうえ、<br>『同意する』にチェックをしていただき、<br
+                        class="mobile">エントリーシートをダウンロードください。</p>
+                <div class="active-policy-scroll mt24">
+                    <div class="active-policy-scroll-inner">
+                        <p class="tac">《外部スタッフエントリーシート、利用する個人情報に関する同意書》</p>
+                        <br>
+                        個人情報の利用目的<br>
+                        外部スタッフのご応募の内容にご対応するため<br>
+                        <br>
+                        取得、利用する個人情報の内容<br>
+                        氏名、住所、電話番号・FAX番号、メールアドレス、<br>
+                        法人・学校名、部署・学部名<br>
+                        <br>
+                        提供に関して<br>
+                        お預かりした個人情報を第三者に提供する事はありません。<br>
+                        <br>
+                        委託に関して<br>
+                        お預かりした個人情報を発送業務等の委託のため契約委託先に委託する場合があります。<br>
+                        <br>
+                        任意性に関して<br>
+                        個人情報をお預け頂く事は任意によるものです。<br>
+                        <br>
+                        開示、訂正、削除に関して<br>
+                        お預かりした個人情報の開示、訂正、削除に関する件は下記までご連絡ください。<br>
+                        <br>
+                        【個人情報に関する問い合わせ先】<br>
+                        近江印刷株式会社　個人情報問合せ窓口　宛<br>
+                        〒529-1302　滋賀県愛知郡愛荘町川原771-1<br>
+                        TEL：<a href="tel:0749428400">0749-42-8400</a>　FAX：0749-42-6700　メール：info@omi-in.co.jp<br>
+                        対応受付時間　AM9:00～PM5:00（当社休業日を除く）
+                    </div>
+                </div>
+                <div class="active-policy-check">
+                    <label>
+                        <span class="wpcf7-form-control-wrap" data-name="privacy">
+                            <span class="wpcf7-form-control wpcf7-acceptance">
+                                <span class="wpcf7-list-item">
+                                    <input class="js-activation-check" type="checkbox" name="privacy" value="1"
+                                        aria-invalid="false">
+                                </span>
+                            </span>
+                        </span>
+                        <span class="active-policy-check-text">個人情報の取り扱いに同意する</span>
+                    </label>
+                </div>
+                <a href="<?php echo home_url('/privacypolicy/'); ?>"
+                    class="active-policy-check-link mt12--sp8">個人情報保護方針</a>
+            </div>
+            <a id="entry-sheet" href="https://www.omi-in.co.jp/pdf/entry_2.pdf" target="_blank"
+                class="active-policy-btn active-policy-btn--partner btn mt64--sp48">
+                <span class="btn__text btn__text--entry js-activation-button">エントリーシートを<br
+                        class="mobile">ダウンロードする</span>
+                <span class="btn__icon">
+                    <svg viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_40_1378)">
+                            <path
+                                d="M13.6814 18.9216L12.2322 17.4304L17.4836 12.1789H0.830322V10.1164H17.4836L12.2322 4.86497L13.6814 3.37378L21.4553 11.1477L13.6814 18.9216Z"
+                                fill="#0068B6" />
+                        </g>
+                    </svg>
+                </span>
+            </a>
+        </div>
+    </div>
+</div>
+
+<?php get_template_part('template-parts/footer'); ?>
