fd700cfb6578f7f889305f75a2cf83a60b3fd75f
HIRAYAMA\reona <belinklab@gmail.com>
2026-09-15 14:55:15 +0900

    LP用/ページ＆ｓｃｓｓ準備

---
+++ page-dtf.php
@@ -0,0 +1,11 @@
+<?php
+if (! defined('ABSPATH')) exit;
+/* 
+Template Name: DTF印刷 LPページ
+*/
+?>
+
+<?php get_template_part('template-parts/header'); ?>
+
+
+<?php get_template_part('template-parts/footer'); ?>
---
+++ page-nicecrew.php
@@ -0,0 +1,11 @@
+<?php
+if (! defined('ABSPATH')) exit;
+/* 
+Template Name: NICECREW LPページ
+*/
+?>
+
+<?php get_template_part('template-parts/header'); ?>
+
+
+<?php get_template_part('template-parts/footer'); ?>
